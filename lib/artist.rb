a0a88448fd579648af882a6fb37ab850d85833f9
Win Feigle <win.feigle@gmail.com>
2022-10-01 16:22:51 -0600

    commit tree 7a354b5ca96d66b8ecaed4327ef1871f0f547c23

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,25 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    # super is used to automatically initialize the method that was written in Memorable::InstanceMethods
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -34,9 +29,5 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
